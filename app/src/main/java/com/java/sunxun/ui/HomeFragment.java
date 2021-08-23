64e7f7c0d9644f36a87d187616a8e2a2dd9e77d6
钱厚德 <2019011238@secoder.net>
2021-08-23 17:06:41 +0800

    Replace ListView with RecyclerView

--- app/src/main/java/com/java/sunxun/ui/HomeFragment.java
+++ app/src/main/java/com/java/sunxun/ui/HomeFragment.java
@@ -1,33 +1,73 @@
 package com.java.sunxun.ui;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.java.sunxun.MainActivity;
 import com.java.sunxun.R;
 import com.java.sunxun.databinding.FragmentHomeBinding;
 import com.java.sunxun.network.ApplicationNetwork;
 import com.java.sunxun.network.NetworkHandler;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 
 public class HomeFragment extends Fragment {
 
     @Nullable
     FragmentHomeBinding binding;
+
+    static class EntityListRecyclerViewAdapter extends RecyclerView.Adapter<EntityListRecyclerViewAdapter.ViewHolder> {
+
+        final private LayoutInflater mInflater;
+        final private String[] data;
+
+        public EntityListRecyclerViewAdapter(Context context) {
+            this.mInflater = LayoutInflater.from(context);
+            this.data = new String[20];
+            for (int i = 0; i < 20; ++i) {
+                this.data[i] = "item" + (i + 1);
+            }
+        }
+
+        @Override
+        public @NotNull ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
+            return new ViewHolder(mInflater.inflate(R.layout.fragment_home, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.itemNameView.setText(data[position]);
+            holder.itemCategoryView.setText(data[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.length;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView itemNameView;
+            public TextView itemCategoryView;
+
+            public ViewHolder(View view) {
+                super(view);
+                this.itemNameView = (TextView) view.findViewById(R.id.entity_name);
+                this.itemCategoryView = (TextView) view.findViewById(R.id.entity_category);
+            }
+        }
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,25 +79,7 @@ public class HomeFragment extends Fragment {
         ApplicationNetwork.getEntityList(new NetworkHandler<String>(this) {
             @Override
             public void onSuccess(String result) {
-                List<Map<String, Object>> entityInfoList = new ArrayList<>();
-
-                // TODO: These data is fake, replace it!
-                String[] entityName = {"定语顺序排列不当", "强加关系或弄错关系", "成分残缺", "状语顺序排列不当", "“与”作复音虚词", "表示推测、估量的语气副词"};
-                String[] entityCategory = {"变调", "变调", "??", "变调", "变调", "变调"};
-
-                for (int i = 0; i < entityName.length; ++i) {
-                    Map<String, Object> entityInfo = new HashMap<>();
-                    entityInfo.put("name", entityName[i]);
-                    entityInfo.put("category", entityCategory[i]);
-                    entityInfoList.add(entityInfo);
-                }
-
-                binding.entityList.setAdapter(new SimpleAdapter(
-                        HomeFragment.this.getActivity(), entityInfoList,
-                        R.layout.home_entity_item,
-                        new String[] {"name", "category"},
-                        new int[] {R.id.entity_name, R.id.entity_category}
-                        ));
+                binding.entityList.setAdapter(new EntityListRecyclerViewAdapter(HomeFragment.this.getActivity()));
             }
 
             @Override
