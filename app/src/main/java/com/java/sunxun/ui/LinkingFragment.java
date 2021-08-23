afbb7a881d06df80949ceba9194c176c2d69f9d3
UNIDY2002 <UNIDY2002@outlook.com>
2021-08-23 17:47:45 +0800

    Render linking response

--- app/src/main/java/com/java/sunxun/ui/LinkingFragment.java
+++ app/src/main/java/com/java/sunxun/ui/LinkingFragment.java
@@ -2,6 +2,10 @@ package com.java.sunxun.ui;
 
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import com.google.android.material.snackbar.Snackbar;
 import com.java.sunxun.databinding.FragmentLinkingBinding;
 import com.java.sunxun.models.Linking;
 import com.java.sunxun.models.Subject;
@@ -23,6 +28,7 @@ public class LinkingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentLinkingBinding.inflate(inflater, container, false);
+        binding.linkingAnswerField.setMovementMethod(LinkMovementMethod.getInstance());
         binding.linkingReturnIcon.setOnClickListener(view -> NavHostFragment.findNavController(this).navigateUp());
         binding.linkingSubmitButton.setOnClickListener(view -> {
             Editable editable = binding.linkingQuestionInput.getText();
@@ -30,7 +36,19 @@ public class LinkingFragment extends Fragment {
                 PlatformNetwork.linking(Subject.chinese, editable.toString(), new NetworkHandler<Linking>(view) {
                     @Override
                     public void onSuccess(Linking result) {
-                        binding.linkingAnswerField.setText(result.getContext());
+                        SpannableString spannableString = new SpannableString(result.getContext());
+                        result.getLinkingResults().forEach(linkingResult -> spannableString.setSpan(
+                                new ClickableSpan() {
+                                    @Override
+                                    public void onClick(@NonNull View widget) {
+                                        Snackbar.make(widget, "Entity uri = " + linkingResult.getEntityUri(), Snackbar.LENGTH_LONG).show();
+                                    }
+                                },
+                                linkingResult.getStartIndex(),
+                                linkingResult.getEndIndex() + 1,
+                                Spanned.SPAN_INCLUSIVE_INCLUSIVE
+                        ));
+                        binding.linkingAnswerField.setText(spannableString);
                     }
 
                     @Override
