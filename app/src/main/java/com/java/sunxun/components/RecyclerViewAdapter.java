a4a2ec5952720b17081e110e416da188e5e7b2ae
钱厚德 <2019011238@secoder.net>
2021-09-03 14:43:43 +0800

    Add RecyclerViewAdapter

---
+++ app/src/main/java/com/java/sunxun/components/RecyclerViewAdapter.java
@@ -0,0 +1,56 @@
+package com.java.sunxun.components;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public abstract class RecyclerViewAdapter<T> extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    protected Context mContext;
+    protected int mLayoutId;
+    protected List<T> mData;
+
+    public RecyclerViewAdapter(Context context, int layoutId, List<T> data) {
+        this.mContext = context;
+        this.mLayoutId = layoutId;
+        this.mData = data;
+    }
+
+    @Override
+    public @NonNull ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater.from(mContext).inflate(mLayoutId, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // TODO: What is this?
+        // holder.updatePosition(position);
+        this.convert(holder, mData.get(position));
+    }
+
+    public abstract void convert(ViewHolder holder, T data);
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        final private View mConvertView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.mConvertView = itemView;
+        }
+
+        public <K extends View> K getViewById(int viewId) {
+            return (K) mConvertView.findViewById(viewId);
+        }
+    }
+}
--- app/src/main/java/com/java/sunxun/ui/HomeFragment.java
+++ app/src/main/java/com/java/sunxun/ui/HomeFragment.java
@@ -1,7 +1,6 @@
 package com.java.sunxun.ui;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,71 +14,40 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.java.sunxun.R;
+import com.java.sunxun.components.RecyclerViewAdapter;
 import com.java.sunxun.databinding.FragmentHomeBinding;
 import com.java.sunxun.models.Entity;
 import com.java.sunxun.models.Subject;
 import com.java.sunxun.network.ApplicationNetwork;
 import com.java.sunxun.network.NetworkHandler;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class HomeFragment extends Fragment {
 
     @Nullable
     FragmentHomeBinding binding;
 
-    private Subject selectedSubject;
     final private List<Entity> baseEntities = new ArrayList<>();
+
+    private Subject selectedSubject = Subject.chinese;
     private int requestCnt = 0;
-    private int entityNumEveryRequest = 10; // TODO: Make it editable later
-
-    class EntityListRecyclerViewAdapter extends RecyclerView.Adapter<EntityListRecyclerViewAdapter.ViewHolder> {
-
-        @Override
-        public @NotNull ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
-            return new ViewHolder(
-                    LayoutInflater.from(parent.getContext()).inflate(R.layout.home_entity_item, parent, false));
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.itemNameView.setText(baseEntities.get(position).getLabel());
-            holder.itemCategoryView.setText(baseEntities.get(position).getCategory());
-        }
-
-        @Override
-        public int getItemCount() {
-            return baseEntities.size();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-
-            public TextView itemNameView;
-            public TextView itemCategoryView;
-
-            public ViewHolder(View view) {
-                super(view);
-                this.itemNameView = (TextView) view.findViewById(R.id.entity_name);
-                this.itemCategoryView = (TextView) view.findViewById(R.id.entity_category);
-            }
-        }
-    }
+    private int entityNumEveryRequest = 15; // TODO: Make it editable later
+    private int randomSeed = 0;
 
     /**
      * This function will refresh entity list & update UI according to data from net.
+     * @param isRefresh When true, it means refreshing. When false, it means loading more.
      */
-    private void refreshEntityList() {
-        ApplicationNetwork.getEntityList(selectedSubject, requestCnt, entityNumEveryRequest, new NetworkHandler<String>(this) {
+    private void updateEntityList(boolean isRefresh) {
+        if (!isRefresh) ++requestCnt;
+        ApplicationNetwork.getEntityList(selectedSubject, requestCnt, entityNumEveryRequest, randomSeed, new NetworkHandler<String>(this) {
             @Override
             public void onSuccess(String result) {
                 JSONArray arr = JSON.parseArray(result);
@@ -89,7 +57,15 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
                     baseEntities.add(newEntity);
                 }
                 binding.entityList.setLayoutManager(new LinearLayoutManager(HomeFragment.this.getActivity()));
-                binding.entityList.setAdapter(new EntityListRecyclerViewAdapter());
+                binding.entityList.setAdapter(new RecyclerViewAdapter<Entity>(
+                        HomeFragment.this.getActivity(), R.layout.home_entity_item, baseEntities
+                ) {
+                    @Override
+                    public void convert(RecyclerViewAdapter.ViewHolder holder, Entity data) {
+                        ((TextView) holder.getViewById(R.id.entity_name)).setText(data.getLabel());
+                        ((TextView) holder.getViewById(R.id.entity_category)).setText(data.getCategory());
+                    }
+                });
             }
 
             @Override
@@ -104,7 +80,13 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
         binding.homeSearchInput.setOnFocusChangeListener((view, b) -> NavHostFragment.findNavController(this).navigate(R.id.nav_search));
         binding.homeHistoryIcon.setOnClickListener(view -> NavHostFragment.findNavController(this).navigate(R.id.nav_history));
 
-        refreshEntityList();
+        binding.entityListWrapper.setEnableRefresh(true);
+        binding.entityListWrapper.setOnRefreshListener(refreshLayout -> updateEntityList(true));
+
+        binding.entityListWrapper.setEnableLoadMore(true);
+        binding.entityListWrapper.setOnLoadMoreListener(refreshLayout -> updateEntityList(false));
+
+        updateEntityList(true);
 
         return binding.getRoot();
     }
@@ -113,11 +95,5 @@ public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefre
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }
-
-    @Override
-    public void onRefresh() {
-        ++requestCnt;
-        refreshEntityList();
     }
 }
