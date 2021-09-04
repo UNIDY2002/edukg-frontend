3dda961ae038595abe77ea116affd216572276b4
UNIDY2002 <UNIDY2002@outlook.com>
2021-09-04 12:15:36 +0800

    Implement a naive star-list UI

--- app/src/main/java/com/java/sunxun/ui/StarFragment.java
+++ app/src/main/java/com/java/sunxun/ui/StarFragment.java
@@ -1,26 +1,51 @@
 package com.java.sunxun.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.google.android.material.snackbar.Snackbar;
 import com.java.sunxun.R;
+import com.java.sunxun.components.RecyclerViewAdapter;
 import com.java.sunxun.databinding.FragmentStarBinding;
 import com.java.sunxun.network.ApplicationNetwork;
 import com.java.sunxun.network.NetworkHandler;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class StarFragment extends Fragment {
 
     @Nullable
     FragmentStarBinding binding;
+
+    private final List<StarItem> starList = new ArrayList<>();
+
+    private void updateStarList(RecyclerViewAdapter<StarItem> adapter) {
+        ApplicationNetwork.getStarList(new NetworkHandler<ArrayList<String>>(this) {
+            @Override
+            public void onSuccess(ArrayList<String> result) {
+                starList.clear();
+                result.forEach(uri -> starList.add(new StarItem(uri, true)));
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(Exception e) {
+                if (binding != null && e.getMessage() != null) {
+                    Snackbar.make(binding.starListRecyclerView, e.getMessage(), Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -36,40 +61,30 @@ public class StarFragment extends Fragment {
             popupMenu.show();
         });
 
-        ApplicationNetwork.isStar("233", new NetworkHandler<Boolean>(this) {
+        RecyclerViewAdapter<StarItem> adapter = new RecyclerViewAdapter<StarItem>(getContext(), R.layout.item_star_entity, starList) {
             @Override
-            public void onSuccess(Boolean result) {
-                Log.d("DEBUG", "isStar: " + result.toString());
-                ApplicationNetwork.star("233", new NetworkHandler<Boolean>(activity) {
+            public void convert(ViewHolder holder, StarItem data) {
+                ((TextView) holder.getViewById(R.id.star_entity_name)).setText(data.uri);
+                ((TextView) holder.getViewById(R.id.star_entity_category)).setText(data.uri);
+                ImageView star = holder.getViewById(R.id.star_entity_star);
+                star.setImageResource(data.star ? R.drawable.ic_star : R.drawable.ic_star_border);
+                star.setOnClickListener(view -> ApplicationNetwork.star(data.uri, new NetworkHandler<Boolean>(view) {
                     @Override
                     public void onSuccess(Boolean result) {
-                        Log.d("DEBUG", "star: " + result.toString());
-                        ApplicationNetwork.getStarList(new NetworkHandler<ArrayList<String>>(activity) {
-                            @Override
-                            public void onSuccess(ArrayList<String> result) {
-                                Log.d("DEBUG", "starLen: " + result.size());
-                                result.forEach(uri -> Log.d("DEBUG", "starContent: " + uri));
-                            }
-
-                            @Override
-                            public void onError(Exception e) {
-                                e.printStackTrace();
-                            }
-                        });
+                        star.setImageResource(result ? R.drawable.ic_star : R.drawable.ic_star_border);
                     }
 
                     @Override
                     public void onError(Exception e) {
-                        e.printStackTrace();
+                        Snackbar.make(view, R.string.network_error, Snackbar.LENGTH_LONG).show();
                     }
-                });
+                }));
             }
+        };
 
-            @Override
-            public void onError(Exception e) {
-                e.printStackTrace();
-            }
-        });
+        binding.starListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.starListRecyclerView.setAdapter(adapter);
+        updateStarList(adapter);
 
         return binding.getRoot();
     }
@@ -78,6 +93,16 @@ public class StarFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private static class StarItem {
+        String uri;
+        boolean star;
+
+        StarItem(String uri, boolean star) {
+            this.uri = uri;
+            this.star = star;
+        }
     }
 
 }
